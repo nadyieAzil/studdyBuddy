93220243fb23a74763885e193c17f97711882d03
Rina Syazana Binti Rahman <2025198913@student.uitm.edu.my>
2025-10-08 21:56:19 +0800

    New update login.

---
+++ src/pages/login.tsx
@@ -0,0 +1,16 @@
+import "./style/welcome.css";
+
+
+const login = () => {
+  return (
+    <div id="welcome-container">
+      <h1 id="title">
+        STUDY <br></br>BUDDY by rina
+      </h1>
+      
+    </div>
+  );
+  
+};
+
+export default login;
